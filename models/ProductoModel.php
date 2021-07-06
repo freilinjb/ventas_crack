4635b9329a0dad0f878ad54db8860aaefbfe9382
Acevedooh <48444981+Acevedooh@users.noreply.github.com>
2021-07-05 21:46:55 -0400

    AQUI CASI PRODUCTO

--- models/ProductoModel.php
+++ models/ProductoModel.php
@@ -1,65 +1,174 @@
-<?php 
+<?php
 
 require_once "Conection.php";
 
+class ProductoModel
+{
 
-class ProductoModel { 
+  static public function getProducto()
+  {
+    $respuesta = Conection::connect()->prepare("
+    SELECT 
+    p.idProducto,
+    p.codigoPro,
+    p.nombrePro , 
+    p.descripcion,
+    pc.precio AS precioCompra,
+    pv.precion AS precioVenta,
+    CASE WHEN p.estado IS TRUE  THEN 'Activo' ELSE 'Inactivo' END AS estado
+  
+    FROM producto p
+    INNER JOIN precio_compra pc ON p.idProducto = pc.idProducto
+    INNER JOIN precio_venta pv ON p.idProducto = pc.idProducto
+            ");
 
-    static public function getData($table, $item, $value) {
-        $data = null;
-        if($item != null) {
-			$data = Conection::connect()->prepare("SELECT * FROM $table WHERE $item = :$item");
-            $data -> bindParam(":".$item, $value, PDO::PARAM_STR);
-            $data -> execute();
-            return $data -> fetchAll();
-            
+    $respuesta->execute();
+    return $respuesta->fetchAll();
+  }
 
-		} else {
-			$data = Conection::connect()->prepare("SELECT * FROM $table");
-            $data -> execute();
-            
-			return $data -> fetchAll();
-        }
-		$data = null;
+
+  static public function getCategoria()
+  {
+    $respuesta = Conection::connect()->prepare("SELECT c.idCategoria,  c.descripcion AS categoria FROM categoria c");
+    $respuesta->execute();
+    return $respuesta->fetchAll();
+  }
+
+  static public function getSubCategoria()
+  {
+    $respuesta = Conection::connect()->prepare("SELECT sc.idSubCategoria, sc.descripcion FROM subcategoria sc");
+    $respuesta->execute();
+    return $respuesta->fetchAll();
+  }
+
+  static public function getMarca()
+  {
+    $respuesta = Conection::connect()->prepare("SELECT m.idMarca, m.marca FROM marca m");
+    $respuesta->execute();
+    return $respuesta->fetchAll();
+  }
+
+  static public function getUnidad()
+  {
+    $respuesta = Conection::connect()->prepare("SELECT u.idUnidad, u.descripcion AS unidad FROM unidad u");
+    $respuesta->execute();
+    return $respuesta->fetchAll();
+  }
+
+
+
+
+
+
+
+
+
+  // static public function getSexo()
+  // {
+  //   $respuesta = Conection::connect()->prepare("SELECT s.idSexo, S.sexo FROM sexo S");
+  //   $respuesta->execute();
+  //   return $respuesta->fetchAll();
+  // }
+
+  static public function registrarProducto($datos)
+  {
+
+    // if(isset($datos["idEmpleado"]) && $datos["idEmpleado"] > 0) {
+    //   echo "existe";
+    // } else {
+    //   echo "no existe";
+    // }
+    // print_r($_POST);die;
+    if (isset($datos["idProducto"]) && $datos["idProducto"] > 0) { /// NUEVO EMPLEADO
+      $respuesta = Conection::connect()->prepare("CALL addProducto (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)");
+      $respuesta->bindParam("1", $datos["idProducto"], PDO::PARAM_INT);
+      $respuesta->bindParam("2", $datos["codigo"], PDO::PARAM_STR);
+      $respuesta->bindParam("3", $datos["nombre"], PDO::PARAM_STR);
+      $respuesta->bindParam("4", $datos["idCategoria"], PDO::PARAM_INT);
+      $respuesta->bindParam("5", $datos["idSubCategoria"], PDO::PARAM_STR);
+      $respuesta->bindParam("6", $datos["idMarca"], PDO::PARAM_STR);
+      $respuesta->bindParam("7", $datos["idUnidad"], PDO::PARAM_STR);
+      $respuesta->bindParam("8", $datos["descripcion"], PDO::PARAM_STR);
+      $respuesta->bindParam("9", $datos["stockIni"], PDO::PARAM_INT);
+      $respuesta->bindParam("10", $datos["stockMini"], PDO::PARAM_BOOL);
+      $respuesta->bindParam("11", $datos["reorden"], PDO::PARAM_STR);
+      $respuesta->bindParam("12", $datos["itbis"], PDO::PARAM_STR);
+      $respuesta->bindParam("13", $datos["precioCompra"], PDO::PARAM_STR);
+      $respuesta->bindParam("14", $datos["precioVenta"], PDO::PARAM_STR);
+      $respuesta->bindParam("15", $datos["estado"], PDO::PARAM_STR);
+
+
+
+
+      $respuesta->execute();
+      return $respuesta->fetch();
+    } else { /// EDITAR EMPLEADO
+      $respuesta = Conection::connect()->prepare("CALL addProducto (?,?,?,?,?,?,?,?,?,?,?,?,?,?)");
+      $respuesta->bindParam("1", $datos["codigo"], PDO::PARAM_STR);
+      $respuesta->bindParam("2", $datos["nombre"], PDO::PARAM_STR);
+      $respuesta->bindParam("3", $datos["idCategoria"], PDO::PARAM_INT);
+      $respuesta->bindParam("4", $datos["idSubCategoria"], PDO::PARAM_STR);
+      $respuesta->bindParam("5", $datos["idMarca"], PDO::PARAM_STR);
+      $respuesta->bindParam("6", $datos["idUnidad"], PDO::PARAM_STR);
+      $respuesta->bindParam("7", $datos["descripcion"], PDO::PARAM_STR);
+      $respuesta->bindParam("8", $datos["stockIni"], PDO::PARAM_INT);
+      $respuesta->bindParam("9", $datos["stockMini"], PDO::PARAM_BOOL);
+      $respuesta->bindParam("10", $datos["reorden"], PDO::PARAM_STR);
+      $respuesta->bindParam("11", $datos["itbis"], PDO::PARAM_STR);
+      $respuesta->bindParam("12", $datos["precioCompra"], PDO::PARAM_STR);
+      $respuesta->bindParam("13", $datos["precioVenta"], PDO::PARAM_STR);
+      $respuesta->bindParam("14", $datos["estado"], PDO::PARAM_STR);
+      $respuesta->execute();
+      return $respuesta->fetch();
     }
+  }
 
-	static public function eliminarProducto($tabla, $datos){
+  // static public function getProducto($item, $value)
+  // {
+  //   if ($item != null) {
+  //     $data = Conection::connect()->prepare("
+  //     SELECT 
+  //     p.idProducto,
+  //     p.nombrePro , 
+  //     p.descripcion,
+  //     pc.precio,
+  //     pv.precion,
+  //     CASE WHEN p.estado IS TRUE  THEN 'Activo' ELSE 'Inactivo' END AS estado
 
-		$stmt = Conection::connect()->prepare("DELETE FROM $tabla WHERE idProducto = :id");
+  //     FROM producto p
+  //     INNER JOIN precio_compra pc ON p.idProducto = pc.idProducto
+  //     INNER JOIN precio_venta pv ON p.idProducto = pc.idProducto
+  //               WHERE $item = :$item");
+  //     $data->bindParam(":" . $item, $value, PDO::PARAM_INT);
+  //     $data->execute();
+  //     return $data->fetch();
+  //   } else {
+  //     $data = Conection::connect()->prepare("
+  //     SELECT * FROM empleado_v");
+  //     $data->execute();
+  //     return $data->fetchAll();
+  //   }
+  // }
 
-		$stmt -> bindParam(":id", $datos, PDO::PARAM_INT);
 
-        return ($stmt -> execute()) ? true : false;
-		$stmt = null;
-	}
+  // static public function getEmpleado($ID)
+  // {
+  //   // echo "hola prueba" . $ID;
+  //   // die;
+  //   $data = Conection::connect()->prepare("
+  //       SELECT * FROM empleado_v
+  //       WHERE idUser = $ID");
 
-    static public function registrarProducto($datos) {
+  //   $data->execute();
+  //   return $data->fetch();
+  // }
 
-        $request = null;
+  static public function eliminarProducto($ID)
+  {
+    // echo "hola prueba";die;
+    $data = Conection::connect()->prepare("CALL eliminarProducto ($ID)");
 
-        if(isset($data["idProducto"])) {
-            $request = Conection::connect()->prepare("CALL registrarProducto(?,?,?,?,?,?,?)");
-
-            $request->bindParam("1", $datos["idProducto"], PDO::PARAM_INT);
-            $request->bindParam("2", $datos["idCategoria"], PDO::PARAM_INT);
-            $request->bindParam("3", $datos["idTipoProducto"], PDO::PARAM_INT);
-            $request->bindParam("4", $datos["idSubTipo"], PDO::PARAM_INT);
-            $request->bindParam("5", $datos["Producto"], PDO::PARAM_STR);
-            $request->bindParam("6", $datos["Descripcion"], PDO::PARAM_STR);
-            $request->bindParam("7", $datos["idEstado"], PDO::PARAM_INT);
-
-        } else {
-            $request = Conection::connect()->prepare("CALL registrarProducto(NULL,?,?,?,?,?,?)");
-
-            $request->bindParam("1", $datos["idCategoria"], PDO::PARAM_INT);
-            $request->bindParam("2", $datos["idTipoProducto"], PDO::PARAM_INT);
-            $request->bindParam("3", $datos["idSubTipo"], PDO::PARAM_INT);
-            $request->bindParam("4", $datos["producto"], PDO::PARAM_STR);
-            $request->bindParam("5", $datos["descripcion"], PDO::PARAM_STR);
-            $request->bindParam("6", $datos["idEstado"], PDO::PARAM_INT);
-        }
-        
-        $request->execute();
-        return $request -> fetch();
-    }
+    $data->execute();
+    return $data->fetch();
+  }
 }
