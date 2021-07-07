0bece0022a2dc5dfa0718171f71e24b20814bc3e
Acevedooh <48444981+Acevedooh@users.noreply.github.com>
2021-07-07 13:20:07 -0400

    provedor y producto por mitad

--- models/CategoriaModel.php
+++ models/CategoriaModel.php
@@ -4,11 +4,11 @@ require_once "Conection.php";
 
 class CategoriaModel
 {
-    static public function getCategoria($idCategoria)
-    {
-        $respuesta = null;
-        if($idCategoria == null) {
-            $respuesta = Conection::connect()->prepare("
+  static public function getCategoria($idCategoria)
+  {
+    $respuesta = null;
+    if ($idCategoria == null) {
+      $respuesta = Conection::connect()->prepare("
             SELECT 
               c.idCategoria, 
               c.descripcion AS categoria,
@@ -16,47 +16,41 @@ class CategoriaModel
               COALESCE(u.user, ' - ')  AS creado_por
           FROM categoria c
           LEFT JOIN user u ON u.idUser = c.creado_por");
-          $respuesta->execute();
-             return $respuesta->fetchAll();
-        } else {
-            $respuesta = Conection::connect()->prepare("
+      $respuesta->execute();
+      return $respuesta->fetchAll();
+    } else {
+      $respuesta = Conection::connect()->prepare("
             SELECT 
                 c.idCategoria,
                 c.descripcion AS categoria, 
                 c.estado  
              FROM categoria c WHERE c.idCategoria = $idCategoria");
-             $respuesta->execute();
-             return $respuesta->fetch();
-        }
+      $respuesta->execute();
+      return $respuesta->fetch();
     }
+  }
 
-    static public function registrarCategoria($datos)
-    {
-  
-      // if(isset($datos["idEmpleado"]) && $datos["idEmpleado"] > 0) {
-      //   echo "existe";
-      // } else {
-      //   echo "no existe";
-      // }
-    //   echo $_SESSION["nombre"];
-        // print_r($datos);
+  static public function registrarCategoria($datos)
+  {
 
-      if (isset($datos["idCategoria"]) && $datos["idCategoria"] > 0) { /// NUEVO EMPLEADO
-        $respuesta = Conection::connect()->prepare("UPDATE categoria c SET c.descripcion = ?, c.estado = ? WHERE c.idCategoria = ?");
-        $respuesta->bindParam("1", $datos["nombre"], PDO::PARAM_STR);
-        $respuesta->bindParam("2", $datos["estado"], PDO::PARAM_BOOL);
-        $respuesta->bindParam("3", $datos["idCategoria"], PDO::PARAM_INT);
-  
-        return$respuesta->execute();
-         //$respuesta->fetch();
-      } else { /// EDITAR EMPLEADO
-        $respuesta = Conection::connect()->prepare("INSERT INTO categoria(descripcion, estado, creado_por) VALUES(?,?,?);");
-        $respuesta->bindParam("1", $datos["nombre"], PDO::PARAM_STR);
-        $respuesta->bindParam("2", $datos["estado"], PDO::PARAM_INT);
-        $respuesta->bindParam("3", $datos["creado_por"], PDO::PARAM_INT);
-  
-        return $respuesta->execute();
-        //  $respuesta->fetch();
-      }
+
+
+    if (isset($datos["idCategoria"]) && $datos["idCategoria"] > 0) { /// NUEVO EMPLEADO
+      $respuesta = Conection::connect()->prepare("UPDATE categoria c SET c.descripcion = ?, c.estado = ? WHERE c.idCategoria = ?");
+      $respuesta->bindParam("1", $datos["nombre"], PDO::PARAM_STR);
+      $respuesta->bindParam("2", $datos["estado"], PDO::PARAM_BOOL);
+      $respuesta->bindParam("3", $datos["idCategoria"], PDO::PARAM_INT);
+
+      return $respuesta->execute();
+      //$respuesta->fetch();
+    } else { /// EDITAR EMPLEADO
+      $respuesta = Conection::connect()->prepare("INSERT INTO categoria(descripcion, estado, creado_por) VALUES(?,?,?);");
+      $respuesta->bindParam("1", $datos["nombre"], PDO::PARAM_STR);
+      $respuesta->bindParam("2", $datos["estado"], PDO::PARAM_INT);
+      $respuesta->bindParam("3", $datos["creado_por"], PDO::PARAM_INT);
+
+      return $respuesta->execute();
+      //  $respuesta->fetch();
     }
+  }
 }
