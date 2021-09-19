7b3fae59e77746a2504487e24607897948526ba0
Acevedooh <48444981+Acevedooh@users.noreply.github.com>
2021-09-18 20:01:01 -0400

    registro de comprobante

--- controllers/ComprobanteController.php
+++ controllers/ComprobanteController.php
@@ -2,9 +2,64 @@
 
 class ComprobanteController
 {
-  static public function geComprobante($idTipoComprobante = null)
+  static public function getTipoComprobante()
+  {
+    $respuesta = ComprobanteModel::getTipoComprobante();
+    return $respuesta;
+  }
+
+
+  static public function getComprobante($idTipoComprobante = null)
   {
     $respuesta = ComprobanteModel::getComprobante($idTipoComprobante);
     return $respuesta;
+  }
+
+
+
+  static public function addComprobante()
+  {
+
+    if (isset($_POST['adquirsicion_comprobante'])) {
+
+      $datos = array(
+
+        'idTipoComprobante' => $_POST['idTipoComprobante'],
+        'sucursal' => $_POST['sucursal'],
+        'vencimiento' => $_POST['vencimiento'],
+        'inicio' => $_POST['inicio'],
+        'final' => $_POST['final'],
+        'secuencia' => $_POST['secuencia'],
+        'estado' => $_POST['estado'],
+
+
+
+      );
+
+
+      $resultado = ComprobanteModel::addComprobante($datos);
+      $_POST = null;
+      if ($resultado > 0) {
+        echo '<script>
+          Swal.fire(
+              "Notificacion!",
+              "Se ha registrado de forma correcta!",
+              "success"
+          );
+          </script>';
+      } else if ($resultado == 0) {
+        echo '<script>
+          Swal.fire(
+              "Notificacion!",
+              "Ah ocurrido un errodo!",
+              "success"
+          );
+          </script>';
+      }
+      // print_r($resultado);
+
+
+
+    }
   }
 }
